14dfa6b1db8ddebfb44739d0f74abb8ef4208658
Willyllem88 <guillemcabre2004@gmail.com>
2024-10-22 19:52:29 +0200

    Removes some comments

--- SupermarketManager/src/main/java/edu/upc/subgrupprop113/supermarketmanager/ShelvingUnit.java
+++ SupermarketManager/src/main/java/edu/upc/subgrupprop113/supermarketmanager/ShelvingUnit.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class ShelvingUnit {
     private int uid;
-    private List<Product> products; // Product on the top will be indexed by 0
+    private List<Product> products; // Product on the floor will be indexed by 0
     private ProductTemperature temperature;
 
     ShelvingUnit(int uid, int height, ProductTemperature temperature) {
@@ -28,5 +28,46 @@ public class ShelvingUnit {
 
     public ProductTemperature getTemperature() { return temperature; }
 
-    public void setTemperature(ProductTemperature temperature) { this.temperature = temperature; }
+    public int getHeight() { return products.size(); }
+
+    public void setTemperature(ProductTemperature newTemperature) { this.temperature = newTemperature; }
+
+    /**
+     * Puts a product in the shelving unit, at the height specified by height
+     *
+     * @param product the product to be inserted
+     * @param height where the product will be placed, being 0 the bottom of the shelving unit
+     * @throws NullPointerException if product is null
+     * @throws IndexOutOfBoundsException if height is out of range
+     */
+    public void addProduct(Product product, int height) {
+        if (product == null) {
+            throw new NullPointerException("Product cannot be null.");
+        }
+        if (height < 0 || height >= products.size()) {
+            throw new IndexOutOfBoundsException("Invalid height: " + height);
+        }
+
+        products.set(height, product);
+    }
+
+    /**
+     * Removes the product at the specified height from the shelving unit.
+     *
+     * @param height the position where the product will be removed, with 0 being the bottom of the shelving unit.
+     * @throws IndexOutOfBoundsException if height is out of range.
+     */
+    public void removeProduct(int height) {
+        if (height < 0 || height >= products.size()) {
+            throw new IndexOutOfBoundsException("Invalid height: " + height);
+        }
+        products.set(height, null);
+    }
+
+    /**
+     * Empties all the shelves of the shelving unit
+     */
+    public void emptyShelvingUnit() {
+        products.replaceAll(ignored -> null);
+    }
 }
