f809a4484649a3a70b2c917a5d3d233f0779bda5
alex.rodriguez.r <alex.542004@gmail.com>
2024-11-10 20:22:57 +0100

    Added tests for BruteForce algorithm and fix BruteForce algorithm

--- SupermarketManager/src/main/java/edu/upc/subgrupprop113/supermarketmanager/BruteForce.java
+++ SupermarketManager/src/main/java/edu/upc/subgrupprop113/supermarketmanager/BruteForce.java
@@ -17,7 +17,7 @@ public class BruteForce implements OrderingStrategy {
 
         this.shelvesHeight = origDistribution.getFirst().getHeight();
         this.maxScore = -1;
-        this.bestDistribution = null;
+        this.bestDistribution = deepCopyShelvingUnits(origDistribution, true);
 
         // Start with each product and build the best arrangement with backtracking
         for (Product startingProduct : products) {
@@ -25,7 +25,7 @@ public class BruteForce implements OrderingStrategy {
                 Set<Product> remainingProducts = new HashSet<>(products);
                 remainingProducts.remove(startingProduct);
 
-                ArrayList<ShelvingUnit> currentDistribution = deepCopyShelvingUnits(origDistribution);
+                ArrayList<ShelvingUnit> currentDistribution = deepCopyShelvingUnits(origDistribution, true);
                 currentDistribution.getFirst().addProduct(startingProduct, this.shelvesHeight - 1);
 
                 placeAllProducts(1, remainingProducts, currentDistribution, 0);
@@ -40,16 +40,16 @@ public class BruteForce implements OrderingStrategy {
     {
         if (remainingProducts.isEmpty()) {
             if (currentScore > maxScore) {
-                this.bestDistribution = deepCopyShelvingUnits(shelves);
+                this.bestDistribution = deepCopyShelvingUnits(shelves, false);
                 this.maxScore = currentScore;
             }
         }
         else {
             ShelvingUnit currentShelf = shelves.get(currentShelfIndex % shelves.size());
-            int height = Math.abs(currentShelfIndex / this.shelvesHeight - this.shelvesHeight - 1);
+            int height = this.shelvesHeight - 1 - (currentShelfIndex / shelves.size());
 
             // Use the findBestProduct function to always attempt placing the best possible product
-            Pair<Product, Double> bestProductPair = findBestProduct(currentShelfIndex, remainingProducts, shelves);
+            Pair<Product, Double> bestProductPair = findBestProduct(currentShelfIndex, remainingProducts, shelves, height);
             Product bestProduct = bestProductPair.getKey();
             double bestSimilarity = bestProductPair.getValue();
 
@@ -70,14 +70,17 @@ public class BruteForce implements OrderingStrategy {
     }
 
     // Finds the best next product to place based on similarity and compatibility with the current shelves
-    private Pair<Product, Double> findBestProduct(int currentShelfIndex, Set<Product> remainingProducts, ArrayList<ShelvingUnit> shelves) {
+    private Pair<Product, Double> findBestProduct(int currentShelfIndex, Set<Product> remainingProducts, ArrayList<ShelvingUnit> shelves, int height) {
         ShelvingUnit currentShelf = shelves.get(currentShelfIndex % shelves.size());
         Product bestProduct = null;
         double bestSimilarity = 0;
 
-        if (currentShelfIndex > 0) {
-            ShelvingUnit previousShelf = shelves.get((currentShelfIndex - 1) % shelves.size());
-            int previousHeight = Math.abs((currentShelfIndex - 1) / this.shelvesHeight - this.shelvesHeight - 1);
+            int previousShelfIndex = getPreviousShelfIndex(currentShelfIndex, height, shelves.size());
+            ShelvingUnit previousShelf = shelves.get(previousShelfIndex % shelves.size());
+            int previousHeight = this.shelvesHeight - 1 - (previousShelfIndex / shelves.size());
+            if (previousHeight < 0 || previousHeight >= this.shelvesHeight) {
+                throw new IndexOutOfBoundsException("Height out of bounds: " + previousHeight);
+            }
             Product previousProduct = previousShelf.getProduct(previousHeight);
 
             for (Product candidate : remainingProducts) {
@@ -89,7 +92,7 @@ public class BruteForce implements OrderingStrategy {
                     }
                 }
             }
-        }
+
 
         return new Pair<>(bestProduct, bestSimilarity);
     }
@@ -100,7 +103,7 @@ public class BruteForce implements OrderingStrategy {
         int nextIndex;
         if (this.shelvesHeight % 2 == 0)
         {
-            if ((currentShelfIndex == n - 1 && height % 2 != 0) || (currentShelfIndex == 0 && height % 2 == 0)) {
+            if ((currentShelfIndex % n == (n - 1) && height % 2 != 0) || (currentShelfIndex % n == 0 && height % 2 == 0)) {
                 nextIndex = currentShelfIndex + n;
             }
             else if (height % 2 != 0) nextIndex = currentShelfIndex + 1;
@@ -108,14 +111,45 @@ public class BruteForce implements OrderingStrategy {
         }
         else
         {
-            if ((currentShelfIndex == n - 1 && height % 2 == 0) || (currentShelfIndex == 0 && height % 2 != 0)) {
+            if ((currentShelfIndex % n == (n - 1) && height % 2 == 0) || (currentShelfIndex % n == 0 && height % 2 != 0)) {
                 nextIndex = currentShelfIndex + n;
             }
             else if (height % 2 == 0) nextIndex = currentShelfIndex + 1;
             else nextIndex = currentShelfIndex - 1;
         }
 
+        if (nextIndex < 0 || nextIndex >= n * shelvesHeight) {
+            throw new IndexOutOfBoundsException("Next index out of bounds: " + nextIndex);
+        }
+
         return nextIndex;
+    }
+
+    private int getPreviousShelfIndex(int currentShelfIndex, int height, int n)
+    {
+        int previousIndex;
+        if (this.shelvesHeight % 2 == 0)
+        {
+            if ((currentShelfIndex % n == (n - 1) && height % 2 == 0) || (currentShelfIndex % n == 0 && height % 2 != 0)) {
+                previousIndex = currentShelfIndex - n;
+            }
+            else if (height % 2 != 0) previousIndex = currentShelfIndex - 1;
+            else previousIndex = currentShelfIndex + 1;
+        }
+        else
+        {
+            if ((currentShelfIndex % n == (n - 1) && height % 2 != 0) || (currentShelfIndex % n == 0 && height % 2 == 0)) {
+                previousIndex = currentShelfIndex - n;
+            }
+            else if (height % 2 == 0) previousIndex = currentShelfIndex - 1;
+            else previousIndex = currentShelfIndex + 1;
+        }
+
+        if (previousIndex < 0 || previousIndex >= n * shelvesHeight) {
+            throw new IndexOutOfBoundsException("Previous index out of bounds: " + previousIndex);
+        }
+
+        return previousIndex;
     }
 
     // Test if a product is compatible with a shelf
@@ -129,11 +163,11 @@ public class BruteForce implements OrderingStrategy {
     }
 
     // Creates a deep copy of the shelving units
-    private ArrayList<ShelvingUnit> deepCopyShelvingUnits(ArrayList<ShelvingUnit> originalShelves) {
+    private ArrayList<ShelvingUnit> deepCopyShelvingUnits(ArrayList<ShelvingUnit> originalShelves, boolean empty) {
         ArrayList<ShelvingUnit> copiedShelves = new ArrayList<>();
         for (ShelvingUnit shelf : originalShelves) {
             ShelvingUnit copy = new ShelvingUnit(shelf); // Using the copy constructor
-            copy.emptyShelvingUnit(); // Ensure the copy has no products
+            if (empty) copy.emptyShelvingUnit(); // Ensure the copy has no products
             copiedShelves.add(copy);
         }
         return copiedShelves;
--- SupermarketManager/src/test/java/edu/upc/subgrupprop113/supermarketmanager/BruteForceTest.java
+++ SupermarketManager/src/test/java/edu/upc/subgrupprop113/supermarketmanager/BruteForceTest.java
@@ -11,6 +11,8 @@ public class BruteForceTest {
     private BruteForce bruteForce;
     private ArrayList<ShelvingUnit> shelvingUnits;
     private Set<Product> products;
+    private RelatedProduct relatedProductAB, relatedProductAC, relatedProductBC, relatedProductAD, relatedProductAE, relatedProductAF, relatedProductBD, relatedProductBE, relatedProductBF, relatedProductCD, relatedProductCE, relatedProductCF, relatedProductDE, relatedProductDF, relatedProductEF;
+    private Product productA, productB, productC, productD, productE, productF;
 
     @Before
     public void setUp() {
@@ -21,16 +23,38 @@ public class BruteForceTest {
         shelvingUnits.add(new ShelvingUnit(1, 3, ProductTemperature.AMBIENT));
         shelvingUnits.add(new ShelvingUnit(2, 3, ProductTemperature.FROZEN));
         shelvingUnits.add(new ShelvingUnit(3, 3, ProductTemperature.REFRIGERATED));
-        shelvingUnits.add(new ShelvingUnit(4, 3, ProductTemperature.AMBIENT));
 
         // Setup Products
         products = new HashSet<>();
-        products.add(new Product("Product A", 10.0f, ProductTemperature.AMBIENT, "path/to/imageA.png"));
-        products.add(new Product("Product B", 15.0f, ProductTemperature.FROZEN, "path/to/imageB.png"));
-        products.add(new Product("Product C", 7.5f, ProductTemperature.REFRIGERATED, "path/to/imageC.png"));
-        products.add(new Product("Product D", 8.0f, ProductTemperature.AMBIENT, "path/to/imageD.png"));
-        products.add(new Product("Product E", 12.0f, ProductTemperature.FROZEN, "path/to/imageE.png"));
-        products.add(new Product("Product F", 9.0f, ProductTemperature.REFRIGERATED, "path/to/imageF.png"));
+        productA = new Product("Product A", 10.0f, ProductTemperature.AMBIENT, "path/to/imageA.png");
+        productB = new Product("Product B", 15.0f, ProductTemperature.FROZEN, "path/to/imageB.png");
+        productC = new Product("Product C", 7.5f, ProductTemperature.REFRIGERATED, "path/to/imageC.png");
+        productD = new Product("Product D", 8.0f, ProductTemperature.AMBIENT, "path/to/imageD.png");
+        productE = new Product("Product E", 12.0f, ProductTemperature.FROZEN, "path/to/imageE.png");
+        productF = new Product("Product F", 9.0f, ProductTemperature.REFRIGERATED, "path/to/imageF.png");
+        products.add(productA);
+        products.add(productB);
+        products.add(productC);
+        products.add(productD);
+        products.add(productE);
+        products.add(productF);
+
+        // Setup Related Products
+        relatedProductAB = new RelatedProduct(productA, productB, 0.4f);
+        relatedProductAC = new RelatedProduct(productA, productC, 0.8f);
+        relatedProductAD = new RelatedProduct(productA, productD, 0.5f);
+        relatedProductAE = new RelatedProduct(productA, productE, 0.3f);
+        relatedProductAF = new RelatedProduct(productA, productF, 0.7f);
+        relatedProductBC = new RelatedProduct(productB, productC, 0.2f);
+        relatedProductBD = new RelatedProduct(productB, productD, 0.6f);
+        relatedProductBE = new RelatedProduct(productB, productE, 0.9f);
+        relatedProductBF = new RelatedProduct(productB, productF, 0.1f);
+        relatedProductCD = new RelatedProduct(productC, productD, 0.4f);
+        relatedProductCE = new RelatedProduct(productC, productE, 0.5f);
+        relatedProductCF = new RelatedProduct(productC, productF, 0.6f);
+        relatedProductDE = new RelatedProduct(productD, productE, 0.2f);
+        relatedProductDF = new RelatedProduct(productD, productF, 0.3f);
+        relatedProductEF = new RelatedProduct(productE, productF, 0.8f);
     }
 
     @Test
@@ -47,7 +71,24 @@ public class BruteForceTest {
             }
         }
         assertEquals("All products should be placed", products.size(), productCount);
+        printFinalDistribution(result);
     }
+
+    private void printFinalDistribution(ArrayList<ShelvingUnit> distribution) {
+        for (int i = 0; i < distribution.size(); i++) {
+            ShelvingUnit shelf = distribution.get(i);
+            System.out.println("Shelving Unit " + shelf.getUid() + " (Temperature: " + shelf.getTemperature() + "):");
+            for (int j = 0; j < shelf.getHeight(); j++) {
+                Product product = shelf.getProduct(j);
+                if (product != null) {
+                    System.out.println("  Shelf position " + j + ": " + product.getName());
+                } else {
+                    System.out.println("  Shelf position " + j + ": Empty");
+                }
+            }
+        }
+    }
+
 
     @Test
     public void testProductsInCompatibleShelves() {
@@ -58,7 +99,7 @@ public class BruteForceTest {
             for (int i = 0; i < shelf.getHeight(); i++) {
                 Product product = shelf.getProduct(i);
                 if (product != null) {
-                    assertSame("Product should be compatible with the shelf", shelf.getTemperature(), product.getTemperature());
+                    assertTrue("Product should be compatible with the shelf", shelf.getTemperature() == product.getTemperature());
                 }
             }
         }
