6e323528893101fd20e9df0a2b5157a31f471bfc
mpenalverguilera <mpenalverguilear@gmail.com>
2024-11-11 19:12:48 +0100

    feature/supermarket definition

--- SupermarketManager/src/main/java/edu/upc/subgrupprop113/supermarketmanager/DomainController.java
+++ SupermarketManager/src/main/java/edu/upc/subgrupprop113/supermarketmanager/DomainController.java
@@ -1,6 +1,7 @@
 package edu.upc.subgrupprop113.supermarketmanager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DomainController {
     private static DomainController instance;
@@ -23,7 +24,84 @@ public class DomainController {
         supermarket.logIn(username, password);
     }
 
+    public void closeApp() {
+
+    }
+
     public void logOut() {
         supermarket.logOut();
     }
+
+    public void importSupermarketConfiguration(String path) {
+
+    }
+
+    public void exportSupermarketConfiguration(String path) {
+
+    }
+
+    public void createSupermarketDistribution(int shelvingUnitsHeight, List<String> temperatures, List<String> quantities) {
+
+    }
+
+    public void sortSupermarketByCatalogProducts(String sortingStrategy) {
+
+    }
+
+    public void sortSupermarketProducts(String sortingStrategy) {
+
+    }
+
+    public void addProductToShelvingUnit(String productId, int height, int shelvingUnitPosition) {
+
+    }
+
+    public void eraseProductFromShelvingUnit(int height, int shelvingUnitPosition) {
+
+    }
+
+    public void swapProductsFromShelvingUnits(int position1, int position2, int height1, int height2) {
+
+    }
+
+    public void modifyShelvingUnitType(int position, String temperatureType) {
+
+    }
+
+    public void addShelvingUnit(int position, String temperature) {
+        
+    }
+
+    public void eraseShelvingUnit(int position) {
+
+    }
+
+    public void swapShelvingUnits(int position1, int position2) {
+
+    }
+
+    public void emptyShelvingUnits(int position) {
+
+    }
+
+    public void createProduct(String productName, String temperature, float price, String imagePath, List<String>RealatedKeyWords) {
+
+    }
+
+    public void eraseProduct(String productId) {
+
+    }
+
+    public void modifyProduct(String productName, String temperature, float price, String imagePath, List<String>RealatedKeyWords) {
+
+    }
+
+    public void modifyProductSimilarity(String productId1, String productId2, float similarity) {
+
+    }
+
+    public void searchProduct(String searchText) {
+        
+    }
+
 }
