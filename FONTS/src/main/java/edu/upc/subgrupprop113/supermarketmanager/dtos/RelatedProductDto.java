81c200c1760bed9a01c39e9396b3bfeb8a9fae8e
mpenalverguilera <mpenalverguilear@gmail.com>
2024-12-04 13:46:09 +0100

    feature/ RelatedProductDto finished

--- FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/dtos/RelatedProductDto.java
+++ FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/dtos/RelatedProductDto.java
@@ -1,4 +1,42 @@
 package edu.upc.subgrupprop113.supermarketmanager.dtos;
 
 public class RelatedProductDto {
+    private float value;
+
+    private String product1;
+
+    private String product2;
+
+    public RelatedProductDto() {
+    }
+
+    public RelatedProductDto(float value, String product1, String product2) {
+        this.value = value;
+        this.product1 = product1;
+        this.product2 = product2;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public String getProduct1() {
+        return product1;
+    }
+
+    public void setProduct1(String product1) {
+        this.product1 = product1;
+    }
+
+    public String getProduct2() {
+        return product2;
+    }
+
+    public void setProduct2(String product2) {
+        this.product2 = product2;
+    }
 }
