3409402ee65bd6047570c3967f43660691ebc188
mpenalverguilera <mpenalverguilear@gmail.com>
2024-12-04 14:30:39 +0100

    comment mappers

--- FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/mappers/RelatedProductMapper.java
+++ FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/mappers/RelatedProductMapper.java
@@ -5,24 +5,53 @@ import edu.upc.subgrupprop113.supermarketmanager.models.RelatedProduct;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
+/**
+ * Mapper class responsible for transforming between {@link RelatedProduct} entities and {@link RelatedProductDto} objects.
+ * <p>
+ * This class provides utility methods for converting single `RelatedProduct` entities or lists of entities
+ * to their corresponding DTO representations.
+ * </p>
+ */
 public class RelatedProductMapper {
-    RelatedProductDto toDto(RelatedProduct relatedProduct) {
+
+    /**
+     * Converts a single {@link RelatedProduct} entity to a {@link RelatedProductDto}.
+     * <p>
+     * If the entity is null, this method returns null. It maps the names of the related products to the DTO
+     * if the products themselves are not null.
+     * </p>
+     *
+     * @param relatedProduct the {@link RelatedProduct} entity to convert.
+     * @return a {@link RelatedProductDto} representing the entity, or null if the input is null.
+     */
+    public RelatedProductDto toDto(RelatedProduct relatedProduct) {
         if (relatedProduct == null) return null;
         RelatedProductDto dto = new RelatedProductDto();
         if (relatedProduct.getProduct1() != null) {
             dto.setProduct1(relatedProduct.getProduct1().getName());
+        } else {
+            dto.setProduct1(null);
         }
-        else dto.setProduct1(null);
         if (relatedProduct.getProduct2() != null) {
             dto.setProduct2(relatedProduct.getProduct2().getName());
+        } else {
+            dto.setProduct2(null);
         }
-        else dto.setProduct2(null);
         return dto;
     }
 
-    List<RelatedProductDto> toDto(List<RelatedProduct> relatedProducts) {
+    /**
+     * Converts a list of {@link RelatedProduct} entities to a list of {@link RelatedProductDto} objects.
+     * <p>
+     * If the list is null, this method returns null. Each entity in the list is converted using
+     * the {@link #toDto(RelatedProduct)} method.
+     * </p>
+     *
+     * @param relatedProducts the list of {@link RelatedProduct} entities to convert.
+     * @return a list of {@link RelatedProductDto} objects, or null if the input list is null.
+     */
+    public List<RelatedProductDto> toDto(List<RelatedProduct> relatedProducts) {
         if (relatedProducts == null) return null;
         List<RelatedProductDto> dtos = new ArrayList<>(relatedProducts.size());
         for (RelatedProduct relatedProduct : relatedProducts) {
@@ -31,3 +60,4 @@ public class RelatedProductMapper {
         return dtos;
     }
 }
+
--- FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/mappers/ShelvingUnitMapper.java
+++ FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/mappers/ShelvingUnitMapper.java
@@ -8,13 +8,31 @@ import edu.upc.subgrupprop113.supermarketmanager.models.ShelvingUnit;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Mapper class for converting between {@link ShelvingUnit} entities and {@link ShelvingUnitDto} objects.
+ */
 public class ShelvingUnitMapper {
+
+    /**
+     * Mapper for handling transformations between {@link Product} entities and {@link ProductDto} objects.
+     */
     private final ProductMapper productMapper;
 
+    /**
+     * Constructs a {@code ShelvingUnitMapper} with the specified {@link ProductMapper}.
+     *
+     * @param productMapper the mapper used to transform products contained in shelving units.
+     */
     public ShelvingUnitMapper(ProductMapper productMapper) {
         this.productMapper = productMapper;
     }
 
+    /**
+     * Converts a {@link ShelvingUnit} entity to a {@link ShelvingUnitDto}.
+     *
+     * @param shelvingUnit the {@link ShelvingUnit} entity to convert.
+     * @return the corresponding {@link ShelvingUnitDto}.
+     */
     public ShelvingUnitDto toDto(final ShelvingUnit shelvingUnit) {
         ShelvingUnitDto dto = new ShelvingUnitDto();
         dto.setUid(shelvingUnit.getUid());
@@ -23,6 +41,12 @@ public class ShelvingUnitMapper {
         return dto;
     }
 
+    /**
+     * Converts a list of {@link ShelvingUnit} entities to a list of {@link ShelvingUnitDto} objects.
+     *
+     * @param shelvingUnits the list of {@link ShelvingUnit} entities to convert.
+     * @return a list of {@link ShelvingUnitDto} objects, or {@code null} if the input list is {@code null}.
+     */
     public List<ShelvingUnitDto> toDto(final List<ShelvingUnit> shelvingUnits) {
         if (shelvingUnits == null) return null;
         List<ShelvingUnitDto> dtos = new ArrayList<>(shelvingUnits.size());
