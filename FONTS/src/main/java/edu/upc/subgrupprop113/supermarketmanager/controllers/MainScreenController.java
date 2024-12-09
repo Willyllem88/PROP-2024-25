866760969e665d959712d7bdfad3d4b6696a632a
mpenalverguilera <mpenalverguilear@gmail.com>
2024-12-09 15:06:12 +0100

    fix/ main screen controller show min possible betwen desired and real

--- FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/controllers/MainScreenController.java
+++ FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/controllers/MainScreenController.java
@@ -3,16 +3,12 @@ package edu.upc.subgrupprop113.supermarketmanager.controllers;
 import edu.upc.subgrupprop113.supermarketmanager.controllers.components.ShelvingUnitController;
 import edu.upc.subgrupprop113.supermarketmanager.controllers.components.TopBarController;
 import edu.upc.subgrupprop113.supermarketmanager.factories.DomainControllerFactory;
-import javafx.animation.TranslateTransition;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.util.Duration;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.util.ArrayList;
@@ -24,8 +20,8 @@ public class MainScreenController {
     private final DomainController domainController = DomainControllerFactory.getInstance().getDomainController();
     private TopBarController topBarController;
 
-    public VBox left_b;
-    public VBox right_b;
+    public VBox leftButtonContainer;
+    public VBox rightButtonContainer;
 
     @FXML
     private HBox topBar;
@@ -55,14 +51,14 @@ public class MainScreenController {
     private void initialize() {
         topBarController = (TopBarController) topBar.getProperties().get("controller");
         leftButton.iconSizeProperty().bind(Bindings.createIntegerBinding(
-                () -> (int) (((left_b.getHeight()*0.8 + left_b.getWidth()*0.2)) * 0.15),
-                left_b.heightProperty(),
-                left_b.widthProperty()
+                () -> (int) ((leftButtonContainer.getHeight()*0.8 + leftButtonContainer.getWidth()*0.2) * 0.15),
+                leftButtonContainer.heightProperty(),
+                leftButtonContainer.widthProperty()
         ));
         rightButton.iconSizeProperty().bind(Bindings.createIntegerBinding(
-                () -> (int) (((left_b.getHeight()*0.8 + left_b.getWidth()*0.2)) * 0.15),
-                right_b.heightProperty(),
-                right_b.widthProperty()
+                () -> (int) ((leftButtonContainer.getHeight()*0.8 + leftButtonContainer.getWidth()*0.2) * 0.15),
+                rightButtonContainer.heightProperty(),
+                rightButtonContainer.widthProperty()
         ));
         reloadShelvingUnits();
         topBarController.setOnImportHandler(_ -> reloadShelvingUnits());
@@ -111,15 +107,15 @@ public class MainScreenController {
 
     private void updateVisibleUnits() {
         shelvingUnitContainer.getChildren().clear();
+        int showingUnits = Math.min(visibleUnits, shelvingUnits.size());
 
-        for (int i = 0; i < visibleUnits; i++) {
+        for (int i = 0; i < showingUnits; i++) {
             int index = (currentIndex + i) % shelvingUnits.size();
-            //TODO: Donar un identificador als fills valis pq si no peta
             shelvingUnitContainer.getChildren().add(shelvingUnits.get(index));
         }
     }
 
-    public void moveShelvingUnitsRight(MouseEvent mouseEvent) {
+    public void moveShelvingUnitsRight() {
         if (shelvingUnits.size() <= visibleUnits) return;
 
         currentIndex = (currentIndex + 1) % shelvingUnits.size();
@@ -127,7 +123,7 @@ public class MainScreenController {
         updateVisibleUnits();
     }
 
-    public void moveShelvingUnitsLeft(MouseEvent mouseEvent) {
+    public void moveShelvingUnitsLeft() {
         if (shelvingUnits.size() <= visibleUnits) return;
 
         currentIndex = (currentIndex - 1 + shelvingUnits.size()) % shelvingUnits.size();
