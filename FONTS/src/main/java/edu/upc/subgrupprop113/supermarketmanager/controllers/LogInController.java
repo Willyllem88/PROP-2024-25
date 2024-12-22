510760fee6f729052f5fa6dd0f081acc66966069
alex.rodriguez.r <alex.542004@gmail.com>
2024-12-22 10:39:08 +0100

    Log in now only accepts letters and numbers

--- FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/controllers/LogInController.java
+++ FONTS/src/main/java/edu/upc/subgrupprop113/supermarketmanager/controllers/LogInController.java
@@ -57,6 +57,9 @@ public class LogInController {
             primaryButtonController.setLabelText("Log In");
             primaryButtonController.setOnClickHandler(_ -> handleLogin());
         }
+
+        restrictTextField(usernameField, "[a-zA-Z0-9]*");
+        restrictTextField(passwordField, "[a-zA-Z0-9]*");
     }
 
     @FXML
@@ -72,6 +75,24 @@ public class LogInController {
             System.out.println(e.getMessage());
             toastLabelController.setErrorMsg(e.getMessage(), 4500);
         }
+    }
+
+    /**
+     * Restricts the text field to the specified regular expression.
+     *
+     * @param textField the text field to restrict.
+     * @param regex     the regular expression to use for validation.
+     */
+    private void restrictTextField(TextField textField, String regex) {
+        TextFormatter<String> formatter = new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            // Allow only numbers and a single decimal point
+            if (newText.matches(regex)) {
+                return change;
+            }
+            return null; // Reject the change
+        });
+        textField.setTextFormatter(formatter);
     }
 
 }
